9729015ee5e709f865671b686fc0ae740fe8cfee
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-01-02 13:47:40 -0500

    added isDefined to switch statement

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -28,7 +28,7 @@ void CPU::step() {
 }
 
 void CPU::debug() {
-    printf("INS: %X \t PC: %X\n", memory->memory[rf.getPC() - 1], rf.getPC());
+    printf("INS: %X \t PC: %X\n", memory->memory[rf.getPC()], rf.getPC());
     printf("-------REGISTERS-------\n");
     printf("A: %X \n", rf.readReg(REG_A, IS_8_BIT));
     printf("B: %X \t\t C: %X\n", rf.readReg(REG_B, IS_8_BIT), rf.readReg(REG_C, IS_8_BIT));
@@ -72,9 +72,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x04: { // INC B
             rf.writeReg(REG_B, rf.readReg(REG_B, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x05: { // DEC B 
             rf.writeReg(REG_B, rf.readReg(REG_B, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x06: { // LD B, u8
             break;
@@ -90,9 +92,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x0C: { // INC C
             rf.writeReg(REG_C, rf.readReg(REG_C, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x0D: { // DEC C
             rf.writeReg(REG_C, rf.readReg(REG_C, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x0E: { // LD C, u8
             break;
@@ -108,9 +112,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x14: { // INC D
             rf.writeReg(REG_D, rf.readReg(REG_D, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x15: { // DEC D
             rf.writeReg(REG_D, rf.readReg(REG_D, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x16: { // LD D, u8
             break;
@@ -126,9 +132,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x1C: { // INC E
             rf.writeReg(REG_E, rf.readReg(REG_E, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x1D: { // DEC E
             rf.writeReg(REG_E, rf.readReg(REG_E, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x1E: { // LD E, u8
             break;
@@ -144,9 +152,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x24: { // INC H
             rf.writeReg(REG_H, rf.readReg(REG_H, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x25: { // DEC H
             rf.writeReg(REG_H, rf.readReg(REG_H, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x26: { // LD H, u8
             break;
@@ -162,9 +172,11 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x2C: { // INC L
             rf.writeReg(REG_L, rf.readReg(REG_L, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x2D: { // DEC L
             rf.writeReg(REG_L, rf.readReg(REG_L, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x2E: { // LD L, u8
             break;
@@ -178,6 +190,7 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x33: { // INC SP
             rf.setSP(rf.getSP() + 1);
+            isDefined = true;
             break;
         } case 0x34: { // INC (HL)
             break;
@@ -195,12 +208,15 @@ void CPU::execute(uint8_t ins){
             break;
         } case 0x3B: { // DEC SP
             rf.setSP(rf.getSP() - 1);
+            isDefined = true;
             break;
         } case 0x3C: { // INC A
             rf.writeReg(REG_A, rf.readReg(REG_A, IS_8_BIT) + 1);
+            isDefined = true;
             break;
         } case 0x3D: { // DEC A
             rf.writeReg(REG_A, rf.readReg(REG_A, IS_8_BIT) - 1);
+            isDefined = true;
             break;
         } case 0x3E: { // LD A, u8
             break;
