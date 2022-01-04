d037a41c9ab392d66e5eb43f3cef64481594c7a2
Matthew Gallant <matt@matthewgallant.me>
2022-01-04 09:26:53 -0500

    Update debugRom utility function

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -4,6 +4,8 @@
 
 #include "utilities.hpp"
 
+#define DEBUG_LINE_ITEMS 16
+
 /**
  * Outputs a vector of rom data in a format similar to hexdump
  * 
@@ -11,20 +13,18 @@
  */
 void debugRom(std::vector<uint8_t> romData) {
 	int lineItems = 0;
-	std::ios init(NULL);
-	init.copyfmt(std::cout);
 	for(auto i: romData) {
 		int value = i;
 		std::cout << std::hex << std::setw(2) << std::setfill('0') << value << " ";
 
 		// Limit hex pairs per line to 16 per line like hexdump
 		lineItems++;
-		if (lineItems == 16) {
+		if (lineItems == DEBUG_LINE_ITEMS) {
 			lineItems = 0;
 			std::cout << std::endl;
 		}
 	}
-	std::cout.copyfmt(init);
+	if (lineItems % DEBUG_LINE_ITEMS != 0) std::cout << std::endl;
 }
 
 /**
