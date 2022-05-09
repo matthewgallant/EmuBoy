efe256641153e728ea97c348ac914d9dd3b8259e
Matthew Gallant <matt@matthewgallant.me>
2022-05-09 16:50:51 -0400

    Clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <vector>
 #include <string>
 #include <thread>
@@ -21,7 +20,9 @@
 
 // Slowtime is 500ms
 #define SLOWTIME 20000
+
 int main() {
+
 	// Get file from environment
 	if (!std::getenv("ROM")) {
 		std::cout << "No ROM found in path. Please set 'ROM' in your path to your ROM file." << std::endl;
@@ -29,15 +30,13 @@ int main() {
 	}
 
 	//Initialize cartridge
-	std::cout << "Loading Cartridge" << std::endl;
 	Cartridge cartridge;
 	std::vector<uint8_t> cartridgeContents = cartridge.getCartridgeContents(0x4000, 0x8000);
 
 	// Print rom hex for debugging
-	debugBytes(cartridgeContents);
+	// debugBytes(cartridgeContents);
 
 	// Create virtual memory
-	std::cout << "Create Memory" << std::endl;
 	Memory memory;
 	memory.write(cartridgeContents, 0x0000);
 
@@ -48,29 +47,23 @@ int main() {
     CPU cpu;
 	cpu.setMemory(&memory);
 
-	// Create virtual ppu
-
-	// PPU test setup (should run a but more than one)
-	//for (int i = 0; i < 80000; i++) {
-	//	ppu.step();
-	//}
-
-
 	// run the cpu cycle in a seperate thread
 	std::thread processor([](CPU *cpu) {
-	Memory *memory = cpu->getMemory();
-	uint8_t i = 0;
-	while(true) {
+		Memory *memory = cpu->getMemory();
+		uint8_t i = 0;
+		while(true) {
 			cpu->debug();
 			cpu->step();
+
 			// write to the first section of memory as indicator 
 			// threads have some shared memory
 			memory->write(i++, VRAM_OFFSET);
+
 			//if(cpu->getInstruction() == 0) continue;
 			if(MODE == SLOW) usleep(SLOWTIME);
 			else if(MODE == RUN) continue;
 			else if(MODE == DEBUG) getchar();
-		}		
+		}
 	}, &cpu);
 	
 	// Create virtual ppu
