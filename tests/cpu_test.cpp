f289cbde45a585a526ee257df43aeb1072b1b3b4
Jacob Mealey <jacobmealey.maine@gmail.com>
2022-05-19 20:38:50 -0400

    initial tests for CPU stuff

---
+++ tests/cpu_test.cpp
@@ -0,0 +1,13 @@
+#include "RegisterFile.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+#include "memory.hpp"
+
+
+TEST_CASE("8-bit Op-Codes :')") {
+    CPU * cpu_t = new cpu;
+    SECTION("Arithmatic") {
+
+
+    }
+}
