eecd289c457843fa9a4c990a13ce19370357704a
Matthew Gallant <matt@matthewgallant.me>
2022-10-13 10:00:36 -0400

    Clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,33 +20,25 @@ fn main() {
 
     // Get and check for arguments
     let args: Vec<String> = env::args().collect();
+
     if args.len() < 2 {
-        println!("No ROM passed, exiting");
+        println!("No ROM passed, exiting...");
         process::exit(1);
     }
 
     // Create memory
     let mut memory = Memory::new();
 
-    // Test setting and reading a single byte
-    memory.set_byte(0x01, 0x00);
-    let byte = memory.byte(0x00);
-    // println!("Byte: {:?}", byte);
-
-    // Test setting and reading multiple bytes
-    memory.set_bytes(vec![0x00, 0x01, 0x02, 0x03], 0x00);
-    let bytes = memory.bytes(0x00, 0x04);
-    // println!("Bytes: {:?}", bytes);
-
-    // Load cartridge from file and get first 32k of contents
+    // Create cartridge from file and get first 32k of contents
     let cart = Cartridge::new(args[1].to_string());
+
+    // Only ask for the second 16k as the first 16k isn't able to be banked and is returned automatically
     let cart_contents = cart.cartridge(0x4000, 0x8000);
-    // println!("Cart contents: {:?}", cart_contents);
 
     // Create LCD
     let mut lcd = Lcd::new();
 
-    // Create event handler (this will be part of the future IO module)
+    // TODO: Move event handler to new IO struct
     let mut event_pump = lcd.context.event_pump().unwrap();
 
     // Create PPU
@@ -59,6 +51,7 @@ fn main() {
         cpu.step(&mut memory);
         ppu.step(&mut memory);
 
+        // TODO: Move event loop to new IO struct
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
